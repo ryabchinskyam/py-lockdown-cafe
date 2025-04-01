43e93c03ca652e9b59b9ee064581c3c0b0f0e1f9
Artem Riabchynskyi <ryabchinskyam@gmail.com>
2025-04-01 22:07:23 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+from datetime import date
+
+from .cafe import Cafe
+from .errors import (
+    NotVaccinatedError,
+    VaccineError,
+    OutdatedVaccineError,
+    NotWearingMaskError,
+)
+
+
+def go_to_cafe(friends: str, cafe: Cafe)-> None:
+    """
+    Функція, яка перевіряє, чи можуть друзі піти в кафе.
+    """
+    not_vaccinated = 0
+    outdated_vaccine = 0
+    not_wearing_mask = 0
+
+    for friend in friends:
+        if "vaccine" not in friend:
+            not_vaccinated += 1
+        elif friend["vaccine"]["expiration_date"] < date.today():
+            outdated_vaccine += 1
+
+        if not friend["wearing_a_mask"]:
+            not_wearing_mask += 1
+
+    if not_vaccinated > 0 or outdated_vaccine > 0:
+        return "All friends should be vaccinated"
+    elif not_wearing_mask > 0:
+        return f"Friends should buy {not_wearing_mask} masks"
+    else:
+        return f"Friends can go to {cafe.name}"
